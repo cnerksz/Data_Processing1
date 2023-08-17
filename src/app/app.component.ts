b60a4f523a9e12befe6dcbdd6d22f783a0d3962a
cnerksz <56205551+cnerksz@users.noreply.github.com>
2023-08-17 08:07:34 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'work1';
-}
+export class AppComponent {}
